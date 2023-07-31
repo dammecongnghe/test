e70d7dcbb6a86c3a984ce2f712dab2927173bf33
ducnm63 <51069344+dautroc1@users.noreply.github.com>
2023-07-31 12:08:29 +0700

    Update test.py

--- test.py
+++ test.py
@@ -59,7 +59,7 @@ def recommend():
     random_proxy = random.choice(proxies)
 
     # Create OpenAI ChatCompletion request
-    response = g4f.ChatCompletion.create(model='gpt-3.5-turbo',proxy=random_proxy, provider=g4f.Provider.EasyChat, messages=[
+    response = g4f.ChatCompletion.create(model='gpt-3.5-turbo', provider=g4f.Provider.DeepAi, messages=[
                                       {"role": "system", "content": "You are an assistant to help recommend movies and shows for people."},
             {"role": "user", "content": 'Movie and show names should always and only one be in quotes in all responses, answer concisely'},
             {"role": "assistant", "content": "Yes, I will do that."},
